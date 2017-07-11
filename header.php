9c4d68706cc043f408e393e3ce5ecf1bb5b14cfd
edent <github.com@shkspr.mobi>
2017-07-11 21:26:27 +0100

    Added header image

--- header.php
+++ header.php
@@ -20,7 +20,7 @@
 	<style>#forkongithub a{background:#000;color:#fff;text-decoration:none;font-family:arial,sans-serif;text-align:center;font-weight:bold;padding:5px 40px;font-size:0.5em;line-height:1em;position:relative;transition:0.5s;}#forkongithub a:hover{background:#c11;color:#fff;}#forkongithub a::before,#forkongithub a::after{content:"";width:100%;display:block;position:absolute;top:1px;left:0;height:1px;background:#fff;}#forkongithub a::after{bottom:1px;top:auto;}@media screen and (min-width:800px){#forkongithub{position:absolute;display:block;top:0;right:0;width:200px;overflow:hidden;height:200px;z-index:9999;}#forkongithub a{width:200px;position:absolute;top:60px;right:-60px;transform:rotate(45deg);-webkit-transform:rotate(45deg);-ms-transform:rotate(45deg);-moz-transform:rotate(45deg);-o-transform:rotate(45deg);box-shadow:4px 4px 10px rgba(0,0,0,0.8);}}</style>
 </head>
 <body>
-	<h1>OpenBenches - ALPHA</h1>
+	<h1><img src="noun_1130095_cc.png" width="50px"> OpenBenches - ALPHA</h1>
 	<span id="forkongithub"><a href="https://github.com/edent/openbenches">Fork me on GitHub</a></span>
 	<div class="button-bar">
 		<a href="index.php" class="hand-drawn">Map View</a>
