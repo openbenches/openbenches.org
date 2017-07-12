b93e2b9d78e081311ccfb6f5056788e777926d6e
edent <github.com@shkspr.mobi>
2017-07-12 07:32:49 +0100

    Better structure

--- footer.php
+++
@@ -1,7 +0,0 @@
-<footer>
-	All photos freely available under <a href="https://creativecommons.org/licenses/by-sa/4.0/">Creative Commons BY-SA</a>.
-	<br>
-	Made with 💖 in Oxford by <a href="https://shkspr.mobi/blog">Terence Eden</a> and <a href="http://mymisanthropicmusings.org.uk/">Elizabeth Eden</a>.
-</footer>
-</body>
-</html>
