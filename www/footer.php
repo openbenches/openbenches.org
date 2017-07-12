2b3266f3206df6a48cb350f41cda176075b130de
edent <github.com@shkspr.mobi>
2017-07-12 20:48:28 +0100

    Add links to blog

--- www/footer.php
+++ www/footer.php
@@ -1,6 +1,8 @@
 <footer>
 	All photos freely available under <a href="https://creativecommons.org/licenses/by-sa/4.0/">Creative Commons BY-SA</a>.
 	<br>
+	<a href="/blog/" class="hand-drawn">Blog</a>	<a href="/blog/about/" class="hand-drawn">About</a>
+	<br>
 	Made with 💖 in Oxford by <a href="https://shkspr.mobi/blog">Terence Eden</a> and <a href="http://mymisanthropicmusings.org.uk/">Elizabeth Eden</a>.
 </footer>
 </body>
