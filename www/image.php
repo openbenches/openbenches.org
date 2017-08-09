950f9663f2df3813586d78a443acb30a26d03c5b
edent <github.com@shkspr.mobi>
2017-08-09 22:01:58 +0100

    Full image returned unaltered
    Fixes #17

--- www/image.php
+++ www/image.php
@@ -10,29 +10,34 @@ $photo_path = "photos/".$directory."/".$subdirectory."/";
 $photo_full_path = $photo_path.$sha1.".jpg";
 
 function imagecreatefromjpegexif($filename)
-	{
-		 $img = imagecreatefromjpeg($filename);
-		 $exif = exif_read_data($filename);
-		 if ($img && $exif && isset($exif['Orientation']))
-		 {
-			$ort = $exif['Orientation'];
+{
+	 $img = imagecreatefromjpeg($filename);
+	 $exif = exif_read_data($filename);
+	 if ($img && $exif && isset($exif['Orientation']))
+	 {
+		$ort = $exif['Orientation'];
 
-			if ($ort == 6 || $ort == 5)
-				$img = imagerotate($img, 270, null);
-			if ($ort == 3 || $ort == 4)
-				$img = imagerotate($img, 180, null);
-			if ($ort == 8 || $ort == 7)
-				$img = imagerotate($img, 90, null);
-			if ($ort == 5 || $ort == 4 || $ort == 7)
-				imageflip($img, IMG_FLIP_HORIZONTAL);
-		 }
-		 return $img;
-	}
-
-$image = imagecreatefromjpegexif($photo_full_path);
-if(null != $size){
-	$image = imagescale($image,$size);
+		if ($ort == 6 || $ort == 5)
+			$img = imagerotate($img, 270, null);
+		if ($ort == 3 || $ort == 4)
+			$img = imagerotate($img, 180, null);
+		if ($ort == 8 || $ort == 7)
+			$img = imagerotate($img, 90, null);
+		if ($ort == 5 || $ort == 4 || $ort == 7)
+			imageflip($img, IMG_FLIP_HORIZONTAL);
+	 }
+	 return $img;
 }
-header("Content-Type: image/jpg");
-imagejpeg($image);
-imagedestroy($image);
+
+if(null != $size){
+	$image = imagecreatefromjpegexif($photo_full_path);
+	$image = imagescale($image, $size);
+	header("Content-Type: image/jpeg");
+	imagejpeg($image);
+	imagedestroy($image);
+} else {
+	//	Return the full image (preserves EXIF)
+	header('Content-type: image/jpeg');
+	readfile($photo_full_path);
+}
+die();
