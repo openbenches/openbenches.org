9edf6d5366c0a4e6ddea1926c80bb9e59bc7e8d7
edent <github.com@shkspr.mobi>
2017-08-12 20:25:33 +0100

    iPhone flipped image fix
    Fixes #64

--- www/image.php
+++ www/image.php
@@ -9,6 +9,25 @@ $photo_full_path = get_path_from_hash($sha1);
 function show_scaled_image($imagePath, $size)
 {
 	$imagick = new \Imagick(realpath($imagePath));
+
+	//	Some phones (mostly iPhones) have rotated images
+	//	Use the EXIF to correct
+	//	http://php.net/manual/en/imagick.getimageorientation.php#111448
+	$orientation = $imagick->getImageOrientation();
+	switch($orientation) {
+		case imagick::ORIENTATION_BOTTOMRIGHT:
+			$imagick->rotateimage("#000", 180); // rotate 180 degrees
+		break;
+
+		case imagick::ORIENTATION_RIGHTTOP:
+			$imagick->rotateimage("#000", 90); // rotate 90 degrees CW
+		break;
+
+		case imagick::ORIENTATION_LEFTBOTTOM:
+			$imagick->rotateimage("#000", -90); // rotate 90 degrees CCW
+		break;
+	}
+
 	$imagick->resizeImage($size, null, Imagick::FILTER_CATROM,1);
 	header("Content-Type: image/jpeg");
 	echo $imagick->getImageBlob();
