5f921c9cded244bbd597a27258ef501786560106
Daniel Pope <danielthepope@users.noreply.github.com>
2017-08-12 23:13:36 +0100

    Add Twitter link in footer

--- www/footer.php
+++ www/footer.php
@@ -1,7 +1,7 @@
 <footer>
 	<hr>
 	<br>
-	<a href="/" class="hand-drawn">Map</a>	<a href="/blog/about/" class="hand-drawn">About</a>	<a href="https://github.com/edent/openbenches" class="hand-drawn">GitHub</a>
+	<a href="/" class="hand-drawn">Map</a>	<a href="/blog/about/" class="hand-drawn">About</a>	<a href="https://twitter.com/openbenches" class="hand-drawn">Twitter</a>	<a href="https://github.com/edent/openbenches" class="hand-drawn">GitHub</a>
 	<br><br>
 	Made with 💖 in Oxford by<br>
 	<a href="https://shkspr.mobi/blog">Terence Eden</a> and <a href="http://mymisanthropicmusings.org.uk/">Elizabeth Eden</a>.
