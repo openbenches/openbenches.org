e0600ea6e0fba0f746f8eb11377c5cd091657643
summerbeth <liz.j.eden@gmail.com>
2017-08-20 11:22:50 +0100

    Corrections to 404 page
    * Removed repeating banner
    * Applied CSS to image to render correctly on small screens
    * Added alt text to image
    * Fixes #78

--- www/404.php
+++ www/404.php
@@ -1,7 +1,6 @@
 <?php
-include("header.php");
 ?>
 <h2>404 - Bench Not Found</h2>
-<img src="/image/f0ce9dd7f357bebaf86609fec57b48394385da0b/600" width="600">
+<img src="/image/f0ce9dd7f357bebaf86609fec57b48394385da0b/600" class="proxy-image" alt="Photograph of a bench with a removed plaque">
 <?php
 	include("footer.php");
