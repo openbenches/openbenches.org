d9761a8e7f4f4476703769a736b2f61305a57281
summerbeth <liz.j.eden@gmail.com>
2017-09-03 09:16:57 +0100

    Added rich social links * Added sameAs properties to footer to improve SEO * fixes #89

--- www/footer.php
+++ www/footer.php
@@ -1,9 +1,13 @@
 <footer>
 	<hr>
 	<br>
-	<a href="/" class="hand-drawn">Map</a>	<a href="/blog/about/" class="hand-drawn">About</a>	<a href="https://twitter.com/openbenches" class="hand-drawn">Twitter</a>	<a href="https://github.com/edent/openbenches" class="hand-drawn">GitHub</a>
+	<a href="/" class="hand-drawn">Map</a>	<a href="/blog/about/" class="hand-drawn">About</a>
+	<span itemscope itemtype="https://schema.org/Organization">
+	  	<a itemprop="sameAs" href="https://www.twitter.com/openbenches" class="hand-drawn">Twitter</a>
+	  	<a itemprop="sameAs" href="https://www.github.com/edent/openbenches" class="hand-drawn">GitHub</a>
+	</span>	
 	<br><br>
-	Made with 💖 in Oxford by<br>
+		Made with 💖 in Oxford by<br>
 	<a href="https://shkspr.mobi/blog">Terence Eden</a> and <a href="http://mymisanthropicmusings.org.uk/">Elizabeth Eden</a>.
 </footer>
 </body>
