b2e9538cb556425f777246bdd238142e55dbceb2
edent <github.com@shkspr.mobi>
2017-09-08 14:14:36 +0100

    Upped limit to 4k

--- www/mysql.php
+++ www/mysql.php
@@ -518,7 +518,7 @@ function get_admin_list()
 {
 	global $mysqli;
 
-	$get_list = $mysqli->prepare("SELECT `benchID`, `inscription` FROM `benches` ORDER BY `benchID` DESC LIMIT 0 , 2048");
+	$get_list = $mysqli->prepare("SELECT `benchID`, `inscription` FROM `benches` ORDER BY `benchID` DESC LIMIT 0 , 4096");
 
 	$get_list->execute();
 	/* bind result variables */
