c2d54d5d843c7f569887143963b586ad056b1524
edent <github.com@shkspr.mobi>
2017-09-11 20:07:19 +0100

    Add search box to the 404 page

--- www/404.php
+++ www/404.php
@@ -2,5 +2,12 @@
 ?>
 <h2>404 - Bench Not Found</h2>
 <img src="/image/f0ce9dd7f357bebaf86609fec57b48394385da0b/600" class="proxy-image" alt="Photograph of a bench with a removed plaque">
+<form action="/search/" enctype="multipart/form-data" method="get">
+	<h2>Search for an inscription</h2>
+	<div>
+		<input type="search" id="inscription" name="search" class="search" value="">
+		<input type="submit" class="hand-drawn" value="Search inscriptions" />
+	</div>
+</form>
 <?php
 	include("footer.php");
