cc0cfa2bea7261a69d8ae77bdf6f38c7d1a7d525
edent <github.com@shkspr.mobi>
2018-01-06 20:29:39 +0000

    Better detection on Flickr

--- www/flickr.php
+++ www/flickr.php
@@ -28,9 +28,18 @@ if ($_GET["flickrID"])
 	$title =     $flickrData->{"photo"}->{"title"}->{"_content"};
 	$description=$flickrData->{"photo"}->{"description"}->{"_content"};
 	$import = "https://www.flickr.com/photos/{$owner}/{$id}";
-	https://www.flickr.com/photos/71256895@N00/3397563245
 
+	//	Get the largest scaled image which is not the original
+	$flickrSizeAPI = "https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key={$flickrKey}&format=json&nojsoncallback=1&photo_id={$flickrID}";
+	$flickrSizeJSON = file_get_contents($flickrSizeAPI);
+	$flickrSizeData = json_decode($flickrSizeJSON);
+	$sizes = $flickrSizeData->{"sizes"}->{"size"};
+	end($sizes);
+	$size = prev($sizes);
+	$large = $size->{"source"};
+	$b64 = base64_encode(file_get_contents($large));
 
+	
 	switch ($license) {
 		case 0:
 			$license = "All Rights Reserved";
@@ -62,16 +71,64 @@ if ($_GET["flickrID"])
 	}
 
 	$original = "https://farm{$farm}.staticflickr.com/{$server}/{$id}_{$o_secret}_o.jpg";
+	// $large = "https://farm{$farm}.staticflickr.com/{$server}/{$id}_{$secret}_h.jpg";
 	echo '<form action="flickr.php" enctype="multipart/form-data" method="post">';
-	echo "<textarea name='inscription' cols='70' rows='6'>{$title} {$description}</textarea><br>";
-	echo "<img src='{$original}' width='512'/><br>";
+	echo "<a class='hand-drawn' name='detectButton' id='detectButton' onclick='sendURLToCloudVision(\"{$b64}\")'>Detect Text</a>";
+	echo '<code style="white-space:pre" id="message"></code>';
+	echo "<textarea name='inscription' id='inscription' cols='70' rows='6'>{$title} {$description}</textarea><br>";
+	echo '<input type="submit" value="Share Bench" /><br>';
+	echo "<a target='_blank' href='{$original}'><img id='flickrimg' src='{$large}' width='512'/></a><br>";
 	echo "<input type='text' name='image'   size='60' value='{$original}'><br>";
 	echo "<input type='text' name='lat'     size='60' value='{$lat}'><br>";
 	echo "<input type='text' name='long'    size='60' value='{$long}'><br>";
 	echo "<input type='text' name='license' size='60' value='{$license}'><br>";
 	echo "<input type='text' name='import'  size='60' value='{$import}'><br>";
-	echo '<input type="submit" value="Share Bench" /></form>';
+	echo "</form>";
+?>
+<script src="/libs/jquery.3.2.1/jquery-3.2.1.min.js"></script>
+<script src="/libs/vision/key.js"></script>
+<script>
+var CV_URL = 'https://vision.googleapis.com/v1/images:annotate?key=' + window.apiKey;
 
+function sendURLToCloudVision (content) {
+	console.log("sending this url" + content);
+	// Strip out the file prefix when you convert to json.
+	var request = {
+		requests: [{
+			image: {
+				content: content
+			},
+			features: [{
+				type: 'TEXT_DETECTION'
+			}]
+		}]
+	};
+
+	$('#message').text('Scanning for text...');
+	console.log("posting " + request);
+	$.post({
+		url: CV_URL,
+		data: JSON.stringify(request),
+		contentType: 'application/json'
+	}).fail(function (jqXHR, textStatus, errorThrown) {
+		$('#message').text('ERRORS: ' + textStatus + ' ' + errorThrown);
+	}).done(displayJSON);
+}
+
+function displayJSON (data) {
+	//	Get the text
+	console.log("display");
+	console.log(data);
+	var contents = data.responses[0].fullTextAnnotation.text;
+	//	Add the detected inscription
+	$('#inscription').val(contents);
+
+	var evt = new Event('results-displayed');
+	evt.results = contents;
+	document.dispatchEvent(evt);
+}
+</script>
+<?php
 	// var_export($flickrData->{"photo"});
 	die();
 } elseif ($_POST != null) {
@@ -108,7 +165,7 @@ if ($_GET["flickrID"])
 			rename($filename, $photo_full_path);
 
 			// Send email
-			mail(NOTIFICATION_EMAIL, "Bench {$benchID}", "{$inscription} https://openbenches.org/{$photo_full_path}");
+			// mail(NOTIFICATION_EMAIL, "Bench {$benchID}", "{$inscription} https://openbenches.org/{$photo_full_path}");
 
 			//	Tweet the bench
  			tweet_bench($benchID, $sha1, $inscription, $lat, $long, $license);
