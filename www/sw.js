016af7f829ce63276800ba2002f5d4f14b7224c6
edent <github.com@shkspr.mobi>
2018-05-19 20:54:07 +0100

    Service Worker fixes
    * Fix #151
    * Helps with #149

--- www/sw.js
+++ www/sw.js
@@ -2,19 +2,17 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('openbenches').then(function(cache) {
      return cache.addAll([
-       '/',
-       '/style.css',
-       '/data.json/'
+       '/style.css'
      ]);
    })
  );
 });
 
 self.addEventListener('fetch', function(event) {
-  console.log(event.request.url);
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
     })
   );
 });
+
