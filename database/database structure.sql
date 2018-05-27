d83052094e2879b4c026378165b705571c4aa37d
edent <github.com@shkspr.mobi>
2018-05-27 20:21:16 +0100

    Updated database for #152

--- database/database structure.sql
+++ database/database structure.sql
@@ -8,6 +8,8 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+CREATE DATABASE IF NOT EXISTS `openbenc_benches` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `openbenc_benches`;
 
 CREATE TABLE `benches` (
   `benchID` bigint(20) NOT NULL,
@@ -50,13 +52,19 @@ CREATE TABLE `licences` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 INSERT INTO `licences` (`shortName`, `longName`, `url`) VALUES
+('CC BY 1.0', 'Creative Commons Attribution 1.0', 'https://creativecommons.org/licenses/by/1.0/'),
 ('CC BY 2.0', 'Creative Commons Attribution 2.0 Generic', 'https://creativecommons.org/licenses/by/2.0/'),
+('CC BY 3.0', 'Creative Commons Attribution 3.0', 'https://creativecommons.org/licenses/by/3.0/'),
+('CC BY 4.0', 'Creative Commons Attribution 4.0', 'https://creativecommons.org/licenses/by/4.0/'),
 ('CC BY-NC 2.0', 'Creative Commons Attribution-NonCommercial 2.0 Generic', 'https://creativecommons.org/licenses/by-nc/2.0/'),
 ('CC BY-NC-ND 2.0', 'Creative Commons Attribution-NonCommercial-NoDerivs 2.0 Generic', 'https://creativecommons.org/licenses/by-nc-nd/2.0/'),
 ('CC BY-NC-SA 2.0', 'Attribution-NonCommercial-ShareAlike 2.0 Generic', 'https://creativecommons.org/licenses/by-nc-sa/2.0/'),
 ('CC BY-ND 2.0', 'Attribution-NoDerivs 2.0 Generic', 'https://creativecommons.org/licenses/by-nd/2.0/'),
+('CC BY-SA 1.0', 'Creative Commons Attribution-ShareAlike 1.0', 'https://creativecommons.org/licenses/by-sa/1.0/'),
 ('CC BY-SA 2.0', 'Creative Commons Attribution-ShareAlike 2.0 Generic ', 'https://creativecommons.org/licenses/by-sa/2.0/'),
-('CC BY-SA 4.0', 'Creative Commons Attribution-ShareAlike 4.0 International', 'https://creativecommons.org/licenses/by-sa/4.0/');
+('CC BY-SA 3.0', 'Creative Commons Attribution-ShareAlike 3.0', 'https://creativecommons.org/licenses/by-sa/3.0/'),
+('CC BY-SA 4.0', 'Creative Commons Attribution-ShareAlike 4.0 International', 'https://creativecommons.org/licenses/by-sa/4.0/'),
+('CC Zero', 'Public Domain Dedication', 'https://creativecommons.org/publicdomain/zero/1.0/');
 
 CREATE TABLE `media` (
   `mediaID` bigint(20) NOT NULL,
@@ -80,6 +88,11 @@ INSERT INTO `media_types` (`shortName`, `longName`, `displayOrder`) VALUES
 ('inscription', 'Close-up of the inscription', 0),
 ('pano', 'Panoramic view', 3),
 ('view', 'View from the bench', 2);
+
+CREATE TABLE `merged_benches` (
+  `benchID` bigint(20) NOT NULL,
+  `mergedID` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `users` (
   `userID` bigint(20) NOT NULL,
@@ -106,16 +119,23 @@ ALTER TABLE `media`
 ALTER TABLE `media_types`
   ADD PRIMARY KEY (`shortName`);
 
+ALTER TABLE `merged_benches`
+  ADD UNIQUE KEY `benchID` (`benchID`),
+  ADD KEY `mergedID` (`mergedID`);
+
 ALTER TABLE `users`
   ADD PRIMARY KEY (`userID`);
 
 
 ALTER TABLE `benches`
   MODIFY `benchID` bigint(20) NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `media`
   MODIFY `mediaID` bigint(20) NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `users`
   MODIFY `userID` bigint(20) NOT NULL AUTO_INCREMENT;
+
 
 ALTER TABLE `benches`
   ADD CONSTRAINT `benches_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`);
@@ -125,6 +145,10 @@ ALTER TABLE `media`
   ADD CONSTRAINT `media_ibfk_2` FOREIGN KEY (`licence`) REFERENCES `licences` (`shortName`),
   ADD CONSTRAINT `media_ibfk_3` FOREIGN KEY (`media_type`) REFERENCES `media_types` (`shortName`),
   ADD CONSTRAINT `media_ibfk_4` FOREIGN KEY (`benchID`) REFERENCES `benches` (`benchID`);
+
+ALTER TABLE `merged_benches`
+  ADD CONSTRAINT `merged_benches_ibfk_1` FOREIGN KEY (`benchID`) REFERENCES `benches` (`benchID`),
+  ADD CONSTRAINT `merged_benches_ibfk_2` FOREIGN KEY (`mergedID`) REFERENCES `benches` (`benchID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
