0ada4486856961c42f5545e77fc4e160704cfd8d
edent <github.com@shkspr.mobi>
2018-10-28 17:15:05 +0000

    Added Logout page
    Need to add a link somewhere.

--- www/index.php
+++ www/index.php
@@ -12,7 +12,7 @@
 
 	//	Available pages
 	$pages = array("bench", "add", "image", "benchimage", "flickr", "edit",
-	               "search", "sitemap.xml", "data.json", "login", 
+	               "search", "sitemap.xml", "data.json", "login", "logout",
 	               "leaderboard", "user");
 
 	if(in_array($params[1], $pages)) {
---
+++ www/logout.php
@@ -0,0 +1,31 @@
+<?php 
+require_once ("config.php");
+require __DIR__ . '/vendor/autoload.php';
+use Auth0\SDK\Auth0;
+use Auth0\SDK\API\Authentication;
+
+$auth0 = new Auth0([
+	'domain' =>        AUTH0_DOMAIN,
+	'client_id' =>     AUTH0_CLIENT_ID,
+	'client_secret' => AUTH0_CLIENT_SECRET,
+	'redirect_uri' =>  AUTH0_CALLBACK,
+]);
+
+// Log out of the local application.
+$auth0->logout();
+
+// Setup the Authentication class with required credentials.
+// No API calls are made on instantiation.
+$auth0_api = new Authentication(AUTH0_DOMAIN);
+
+// Get the Auth0 logout URL to end the Auth0 session as well.
+$auth0_logout = $auth0_api->get_logout_link(
+
+    // This needs to be saved in the "Allowed Logout URLs" field in your Application settings.
+   AUTH0_LOGOUT_RETURN_URL,
+    // Indicate the specific Application.
+    AUTH0_CLIENT_ID
+);
+
+header('Location: ' . $auth0_logout);
+exit;
