5be5a163408cec4cec062221df425703af878c3d
mike <mike@continuity.lan>
2018-11-04 19:06:03 +0000

    #171 add rss feed

--- www/index.php
+++ www/index.php
@@ -13,7 +13,7 @@
 	//	Available pages
 	$pages = array("bench", "add", "image", "benchimage", "flickr", "edit",
 	               "search", "sitemap.xml", "data.json", "login", "logout",
-	               "leaderboard", "user");
+	               "leaderboard", "user", "rss");
 
 	if(in_array($params[1], $pages)) {
 		include($params[1].".php");
