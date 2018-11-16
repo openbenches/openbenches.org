0b3a7aaa5430a756976ba0b3022068360ef0b040
edent <github.com@shkspr.mobi>
2018-11-16 08:55:04 +0000

    More 404 images

--- www/404.php
+++ www/404.php
@@ -1,6 +1,10 @@
 <?php
 $broken_images = array("f0ce9dd7f357bebaf86609fec57b48394385da0b",
-                       "5bcbaa4e7f2e30810c2bb81125b57dbcd957577f");
+                       "5bcbaa4e7f2e30810c2bb81125b57dbcd957577f",
+							  "ab2cfeb3fbcd0e53b55c0f28dcb5aaecc7887f47",
+							  "ba85b02ef55f23802b77e44a9895373849f8e8b7",
+							  "607aacd26ffe46460e8d64025d53af064d09dbbb",
+                       "19a831e25b0dee061e8e68c98d0670ccf1338ab5");
 $broken_image = $broken_images[array_rand($broken_images,1)];
 ?>
 <h2>404 - Bench Not Found</h2>
