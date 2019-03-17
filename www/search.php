b2ffc91e652b57fd379cb11d8f9ff32c1949d95b
edent <github.com@shkspr.mobi>
2019-03-17 16:22:13 +0000

    Better styling for search results

--- www/search.php
+++ www/search.php
@@ -20,7 +20,8 @@ if (null != $query)
 	$resultsHTML = "<ul>";
 	foreach ($results as $key => $value) {
 		$thumb = get_image_thumb($key);
-		$thumb_html = "<img src=\"{$thumb}\" class=\"search-thumb\">";
+		$thumb_width = IMAGE_THUMB_SIZE;
+		$thumb_html = "<img src=\"{$thumb}\" class=\"search-thumb\" width=\"{$thumb_width}\">";
 		$resultsHTML .= "<li><a href='/bench/{$key}'>{$thumb_html}{$value}</a></li>";
 	}
 	$resultsHTML .="</ul>";
