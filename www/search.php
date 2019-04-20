7ccfe15425cad07f08f587f298dfa6dcdedcb15a
edent <github.com@shkspr.mobi>
2019-04-20 22:38:38 +0100

    Fix alt tags on search

--- www/search.php
+++ www/search.php
@@ -26,7 +26,7 @@ if (null != $query)
 		foreach ($results as $key => $value) {
 			$thumb = get_image_thumb($key);
 			$thumb_width = IMAGE_THUMB_SIZE;
-			$thumb_html = "<img src=\"{$thumb}\" class=\"search-thumb\" width=\"{$thumb_width}\">";
+			$thumb_html = "<img src=\"{$thumb}\" class=\"search-thumb\" width=\"{$thumb_width}\" alt=\"\">";
 			$resultsHTML .= "<li><a href='/bench/{$key}'>{$thumb_html}{$value}</a></li>";
 		}
 		$resultsHTML .="</ul></div>";
