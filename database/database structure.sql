a61eb3c44780f68fe375e2939c84af63f5619f0f
Terence Eden <github.com@shkspr.mobi>
2019-05-27 07:44:34 +0100

    Update Database Definition

--- database/database structure.sql
+++ database/database structure.sql
@@ -3,38 +3,24 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `openbenc_benches`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `benches`
---
 
 CREATE TABLE `benches` (
   `benchID` bigint(20) NOT NULL,
   `latitude` float(10,6) NOT NULL,
   `longitude` float(10,6) NOT NULL,
-  `address` text COLLATE utf8mb4_unicode_ci,
+  `address` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `inscription` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `present` tinyint(1) NOT NULL,
   `published` tinyint(1) NOT NULL,
-  `added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `added` datetime NOT NULL DEFAULT current_timestamp(),
   `userID` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Triggers `benches`
---
 DELIMITER $$
 CREATE TRIGGER `save_benches_history` BEFORE UPDATE ON `benches` FOR EACH ROW INSERT INTO benches_history
 SELECT *, NOW() 
@@ -43,41 +29,25 @@ WHERE benchID = OLD.benchID
 $$
 DELIMITER ;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `benches_history`
---
-
 CREATE TABLE `benches_history` (
   `benchID` bigint(20) NOT NULL,
   `latitude` float(10,6) NOT NULL,
   `longitude` float(10,6) NOT NULL,
   `inscription` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `address` text COLLATE utf8mb4_unicode_ci,
+  `address` text COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `present` tinyint(1) NOT NULL,
   `published` tinyint(1) NOT NULL,
-  `added` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `added` datetime NOT NULL DEFAULT current_timestamp(),
   `userID` bigint(20) NOT NULL,
-  `changed` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `changed` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `licences`
---
 
 CREATE TABLE `licences` (
   `shortName` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'CC BY-SA 4.0',
   `longName` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'Creative Commons Attribution-ShareAlike 4.0 International',
   `url` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'https://creativecommons.org/licenses/by-sa/4.0/'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `licences`
---
 
 INSERT INTO `licences` (`shortName`, `longName`, `url`) VALUES
 ('CC BY 1.0', 'Creative Commons Attribution 1.0', 'https://creativecommons.org/licenses/by/1.0/'),
@@ -94,12 +64,6 @@ INSERT INTO `licences` (`shortName`, `longName`, `url`) VALUES
 ('CC BY-SA 4.0', 'Creative Commons Attribution-ShareAlike 4.0 International', 'https://creativecommons.org/licenses/by-sa/4.0/'),
 ('CC Zero', 'Public Domain Dedication', 'https://creativecommons.org/publicdomain/zero/1.0/');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `media`
---
-
 CREATE TABLE `media` (
   `mediaID` bigint(20) NOT NULL,
   `benchID` bigint(20) NOT NULL,
@@ -110,21 +74,11 @@ CREATE TABLE `media` (
   `media_type` varchar(32) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `media_types`
---
-
 CREATE TABLE `media_types` (
   `shortName` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
   `longName` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
   `displayOrder` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `media_types`
---
 
 INSERT INTO `media_types` (`shortName`, `longName`, `displayOrder`) VALUES
 ('360', 'VR 360 panoramic ', 4),
@@ -133,22 +87,10 @@ INSERT INTO `media_types` (`shortName`, `longName`, `displayOrder`) VALUES
 ('pano', 'Panoramic view', 3),
 ('view', 'View from the bench', 2);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `merged_benches`
---
-
 CREATE TABLE `merged_benches` (
   `benchID` bigint(20) NOT NULL,
   `mergedID` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `userID` bigint(20) NOT NULL,
@@ -157,33 +99,18 @@ CREATE TABLE `users` (
   `name` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `benches`
---
 ALTER TABLE `benches`
   ADD PRIMARY KEY (`benchID`),
   ADD KEY `contributor` (`userID`);
 
---
--- Indexes for table `benches_history`
---
 ALTER TABLE `benches_history`
   ADD KEY `benchID` (`benchID`),
   ADD KEY `userID` (`userID`);
 
---
--- Indexes for table `licences`
---
 ALTER TABLE `licences`
   ADD PRIMARY KEY (`shortName`);
 
---
--- Indexes for table `media`
---
 ALTER TABLE `media`
   ADD PRIMARY KEY (`mediaID`),
   ADD KEY `contributorID` (`userID`),
@@ -191,76 +118,40 @@ ALTER TABLE `media`
   ADD KEY `media_type` (`media_type`),
   ADD KEY `benchID` (`benchID`);
 
---
--- Indexes for table `media_types`
---
 ALTER TABLE `media_types`
   ADD PRIMARY KEY (`shortName`);
 
---
--- Indexes for table `merged_benches`
---
 ALTER TABLE `merged_benches`
   ADD UNIQUE KEY `benchID` (`benchID`),
   ADD KEY `mergedID` (`mergedID`);
 
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`userID`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `benches`
---
 ALTER TABLE `benches`
   MODIFY `benchID` bigint(20) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `media`
---
 ALTER TABLE `media`
   MODIFY `mediaID` bigint(20) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
   MODIFY `userID` bigint(20) NOT NULL AUTO_INCREMENT;
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `benches`
---
 ALTER TABLE `benches`
   ADD CONSTRAINT `benches_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`);
 
---
--- Constraints for table `benches_history`
---
 ALTER TABLE `benches_history`
   ADD CONSTRAINT `benches_history_ibfk_1` FOREIGN KEY (`benchID`) REFERENCES `benches` (`benchID`),
   ADD CONSTRAINT `benches_history_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`);
 
---
--- Constraints for table `media`
---
 ALTER TABLE `media`
   ADD CONSTRAINT `media_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`),
   ADD CONSTRAINT `media_ibfk_2` FOREIGN KEY (`licence`) REFERENCES `licences` (`shortName`),
   ADD CONSTRAINT `media_ibfk_3` FOREIGN KEY (`media_type`) REFERENCES `media_types` (`shortName`),
   ADD CONSTRAINT `media_ibfk_4` FOREIGN KEY (`benchID`) REFERENCES `benches` (`benchID`);
 
---
--- Constraints for table `merged_benches`
---
 ALTER TABLE `merged_benches`
   ADD CONSTRAINT `merged_benches_ibfk_1` FOREIGN KEY (`benchID`) REFERENCES `benches` (`benchID`),
   ADD CONSTRAINT `merged_benches_ibfk_2` FOREIGN KEY (`mergedID`) REFERENCES `benches` (`benchID`);
