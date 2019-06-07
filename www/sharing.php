727bebad0882d7b31e270cdccdebfab608621707
Terence Eden <github.com@shkspr.mobi>
2019-06-07 09:38:42 +0100

    Use sharing api where available

--- www/sharing.php
+++ www/sharing.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 $URL = urlencode("https://" . $_SERVER['SERVER_NAME'] .  $_SERVER['REQUEST_URI']);
 
@@ -9,16 +9,18 @@ $email     = "mailto:?&body={$URL}";
 $whatsapp  = "https://api.whatsapp.com/send?text={$URL}";
 $telegram  = "https://telegram.me/share/url?url={$URL}";
 ?>
-<fieldset id="sharing">
-	
+<button class="button buttonColour" type="button" id="shareButton">📤 Share this bench</button>
+
+<fieldset id="sharing" style="display:none;">
+
 	<a href="<?php echo $facebook; ?>" target="_blank">
 		<img src="/images/svg/facebook.svg" class="share" alt="Share on Facebook"/>
 	</a>
-	
+
 	<a href="<?php echo $twitter; ?>" target="_blank">
 		<img src="/images/svg/twitter.svg" class="share" alt="Share on Twitter"/>
 	</a>
-	
+
 	<a href="<?php echo $pinterest; ?>" target="_blank">
 		<img src="/images/svg/pinterest.svg" class="share" alt="Share on Pinterest"/>
 	</a>
@@ -26,13 +28,31 @@ $telegram  = "https://telegram.me/share/url?url={$URL}";
 	<a href="<?php echo $whatsapp; ?>" target="_blank">
 		<img src="/images/svg/whatsapp.svg" class="share" alt="Share on Whatsapp"/>
 	</a>
-	
+
 	<a href="<?php echo $email; ?>" target="_blank">
 		<img src="/images/svg/gmail.svg" class="share" alt="Share on email"/>
 	</a>
-	
+
 	<a href="<?php echo $telegram; ?>" target="_blank">
 		<img src="/images/svg/telegram.svg" class="share" alt="Share on Telegram"/>
 	</a>
 
 </fieldset>
+
+
+<script>
+document.getElementById("shareButton").addEventListener('click', event => {
+	if (navigator.share) {
+		navigator.share({
+			url: ''
+		}).then(() => {
+			console.log('Thanks for sharing!');
+		})
+		.catch(console.error);
+	} else {
+		console.log('No native sharing!');
+		document.getElementById("shareButton").style.display="none";
+		document.getElementById("sharing").style.display="block";
+	}
+});
+</script>
