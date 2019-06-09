42a775bcd99f49914244f029ae58047fa0478648
Terence Eden <github.com@shkspr.mobi>
2019-06-09 15:02:27 +0100

    Better layout
    Fixes #91

--- www/bench.php
+++ www/bench.php
@@ -27,7 +27,7 @@ if ($benchAddress == null){
 		$tags = get_tags_from_bench($benchID);
 		if(!empty($tags)){
 			echo "<ul>";
-				echo "<li class='pseudo button'>Tags:</li>";
+			echo    "<li class='pseudo button'>Tags:</li>";
 
 			foreach ($tags as $tag) {
 				echo "<li class='pseudo button'><a href='/tag/{$tag}/'>#{$tag}</a></li>";
