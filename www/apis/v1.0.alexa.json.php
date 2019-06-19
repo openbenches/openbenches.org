dfa941ea92ec109b72d2ee67a12d52938c46d296
Terence Eden <github.com@shkspr.mobi>
2019-06-19 19:01:50 +0100

    Tidy up of Alexa API

--- www/apis/v1.0.alexa.json.php
+++ www/apis/v1.0.alexa.json.php
@@ -2,23 +2,23 @@
 ini_set( 'serialize_precision', -1 );	//	https://stackoverflow.com/questions/42981409/php7-1-json-encode-float-issue
 require_once ('mysql.php');
 
+//	Raw JSON or padded
 if( isset($_GET["format"]) )    { $format    = $_GET["format"]; }    else { $format    = null;}
 
 if( isset($_GET["random"]) )  {
 	list ($benchID, $benchLat, $benchLong, $benchAddress, $benchInscription, $published) = get_random_bench();
 	$speech = array('speech' => "{$benchInscription}. Located at {$benchAddress}" );
 }
-else if( isset($_GET["count"])  )  {
+else if( isset($_GET["count"]) ) {
 	$count = get_bench_count();
 	$formatter = new NumberFormatter("en-gb", NumberFormatter::SPELLOUT);
 	$count_english = $formatter->format($count);
 	$speech = array('speech' => "There are currently {$count_english} benches on the website.");
 }
-else if( isset($_GET["latest"]) )  {
+else if( isset($_GET["latest"]) ) {
 	list ($benchID, $benchLat, $benchLong, $benchAddress, $benchInscription, $published) = get_latest_bench();
 	$speech = array('speech' => "{$benchInscription}. Located at {$benchAddress}" );
 }
-
 
 if ("raw" == $format) {
 	header('Content-Type: application/json; charset=utf-8');
