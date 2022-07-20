473a7c1d8656db9583059bd41b35fe855d48ef24
Terence Eden <github.com@shkspr.mobi>
2022-07-20 13:47:19 +0100

    Download all on main page

--- www/menu.php
+++ www/menu.php
@@ -3,5 +3,6 @@
   <li><a href="/blog/about/">About</a></li>
   <li><a href="/leaderboard/">Leader Board</a></li>
   <li><a href="/colophon/">Colophon</a></li>
-  <li><a href="/add/" class="addBench"><strong>+</strong> Add bench</a></li>
+  <li><a href="/add/">Add bench</a></li>
+  <li><a href="/api/v1.0/data.json/?truncated=false&format=raw&media=true">Download all data in GeoJSON</a></li>
 </ul>
