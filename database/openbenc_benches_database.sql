a2e19afa7e82e265f78176d658d03f106b0dc39a
Terence Eden <github.com@shkspr.mobi>
2023-09-03 10:41:57 +0100

    Update all database dumps

--- database/openbenc_benches_database.sql
+++ database/openbenc_benches_database.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.11
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 13, 2023 at 10:02 PM
--- Server version: 10.3.38-MariaDB-log-cll-lve
--- PHP Version: 7.4.33
+-- Generation Time: Sep 03, 2023 at 10:36 AM
+-- Server version: 10.6.14-MariaDB-cll-lve-log
+-- PHP Version: 8.1.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
