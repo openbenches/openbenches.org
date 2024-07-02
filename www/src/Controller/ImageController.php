7117a0d2e5b6e023bb7f5559728e4bb892c30101
Terence Eden <git@shkspr.mobi>
2024-07-02 15:51:08 +0100

    Prevent image errors

--- www/src/Controller/ImageController.php
+++ www/src/Controller/ImageController.php
@@ -9,15 +9,26 @@ use Symfony\Component\HttpFoundation\Request;
 
 class ImageController extends AbstractController
 {
-	#[Route(["/image/{sha1}", "/media/{sha1}"], name: 'show_media')]
+	                                           // Old oEmbed sizing
+	#[Route(["/image/{sha1}", "/media/{sha1}", "/image/{sha1}/{size}"], name: 'show_media')]
 	public function show_media( string $sha1 ): BinaryFileResponse {
 		//	Files are stored according to their hash
 		//	So "abc123" is stored as "/a/b/abc123.jpg"
 		$directory = substr( $sha1, 0, 1);
 		$subdirectory = substr( $sha1, 1, 1);
 		$photo_path = "photos/" . $directory . "/" . $subdirectory . "/";
+
+		//	Sometimes, broken clients send `/media/abc123.jpg`
+		//	This makes sure any errant file extension is stripped off.
+		$sha1  = explode( ".", $sha1 )[0];
+
 		$photo_full_path = $photo_path . $sha1 . ".jpg";
-		$response = new BinaryFileResponse($photo_full_path);
-		return $response;
+		try {
+			$response = new BinaryFileResponse( $photo_full_path );
+			return $response;
+		} catch (\Exception $e) {
+			//	Generate an HTTP 404 response
+			throw $this->createNotFoundException( "The image does not exist" );
+		}
 	}
 }
