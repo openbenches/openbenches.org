949dc88bb2d9d3999db1c82073ee20ca118215b0
Terence Eden <git@shkspr.mobi>
2025-01-13 17:49:22 +0000

    Add /comments for a list of all comments
    Moved Commentics folder to accomodate

---
+++ www/src/Controller/CommentsController.php
@@ -0,0 +1,60 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Tools\DsnParser;
+
+class CommentsController extends AbstractController
+{
+	#[Route('/comments', name: 'comments_list')]
+	public function comments_list(): Response {
+
+		$commenticsConfig = $_SERVER["DOCUMENT_ROOT"] . "/public/commentics/config.php";
+
+		if ( file_exists( $commenticsConfig ) ) {
+			require_once $commenticsConfig;
+		} else {
+			return $this->render('comments.html.twig', [
+				"comments"       => []
+			]);
+		}
+
+		$commenticsDB="mysqli://" . CMTX_DB_USERNAME . ":" . CMTX_DB_PASSWORD . "@" . CMTX_DB_HOSTNAME . ":" . CMTX_DB_PORT . "/" . CMTX_DB_DATABASE . "?&charset=utf8mb4";
+
+		$dsnParser = new DsnParser();
+		$connectionParams = $dsnParser->parse( $commenticsDB );
+		$conn = DriverManager::getConnection($connectionParams);
+
+
+		$sql = "SELECT   p.url AS page_id, 
+					     c.comment
+				FROM     comments c
+				JOIN     pages p
+				ON       c.page_id = p.id
+				WHERE    c.is_approved = 1  
+				ORDER BY c.date_added 
+				DESC 
+				LIMIT 1000;";
+
+		$stmt = $conn->prepare($sql);
+		$results = $stmt->executeQuery();
+		$results_array = $results->fetchAssociative();
+
+		while ( ( $row = $results->fetchAssociative() ) !== false) {
+			//	Add the details to the array
+			$comments_array[$row["page_id"]] = array(
+				"page_id"     => $row["page_id"],
+				"comment"     => $row["comment"],
+			);
+		}
+
+		//	Render the page
+		return $this->render('comments.html.twig', [
+			"comments"       => $comments_array,
+		]);
+	}
+}
