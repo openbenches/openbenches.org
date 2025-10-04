be43999b6ea56e8f9f6c43c0f1cca7278954e172
Terence Eden <git@shkspr.mobi>
2025-10-04 09:25:46 +0100

    Add OSM export script

---
+++ database/getOSM.py
@@ -0,0 +1,48 @@
+# /// script
+# dependencies = [
+# 	"requests",
+# ]
+# ///
+
+#	This Python script does the following:
+#	Queries OpenStreetMap for all the nodes which have an *existing* OpenBenches ID.
+#	Saves the resultant JSON.
+#	Converts the JSON into a MySQL query to update existing benches with the osmID.
+#	The output must be manually copied into MySQL. Sorry later me!
+#	Run with:
+#		uv run getOSM.py
+
+import requests
+import json
+
+#	Prepare the request.
+payload = {"data": '[out:json][timeout:25];nwr["openbenches:id"](-90,-180,90,180);out geom;'}
+
+#	Send the request to OSM.
+r = requests.post('https://overpass-api.de/api/interpreter', data=payload)
+
+#	Turn it to data.
+data = r.json()
+
+#	Save it as a nicely formatted document.
+file = open("OSM.json", "w")
+file.write( json.dumps( json.loads( r.text ), indent=4) )
+file.close()
+
+#	Read each item.
+for element in data["elements"]:
+	#	Is this a node?
+	if element["type"] != "node":
+		continue
+	#	Set the ID.
+	osmID = element["id"]
+	#	Is the OpenBenches tag is present?
+	if "tags" not in element:
+		continue
+	if "openbenches:id" not in element["tags"]:
+		continue
+	benchID = element["tags"]["openbenches:id"]
+	#	Is it a numeric ID (some aren't!)
+	if benchID.isdigit():
+		#	Output the SQL syntax.
+		print( f"UPDATE `benches` SET `osmID` = '{osmID}' WHERE `benches`.`benchID` = {benchID}; ")
