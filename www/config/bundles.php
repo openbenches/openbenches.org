ed4e9e2edac6c1f7220cf6ced40d994115881381
Terence Eden <git@shkspr.mobi>
2026-02-14 21:37:42 +0000

    Fix bundle stuff

--- www/config/bundles.php
+++ www/config/bundles.php
@@ -4,7 +4,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Auth0\Symfony\Auth0Bundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-];
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true]
+]
