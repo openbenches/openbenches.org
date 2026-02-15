702425e2f5e19bb02eca7b45bb807900f7fa1d57
Terence Eden <git@shkspr.mobi>
2026-02-15 11:18:32 +0000

    Update composer

--- www/config/bundles.php
+++ www/config/bundles.php
@@ -4,5 +4,7 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Auth0\Symfony\Auth0Bundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true]
-]
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+];
